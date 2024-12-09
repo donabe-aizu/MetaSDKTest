155e02fba3fd2ceae76821e83b272d58383c343d
donabe <donabe722@gmail.com>
2024-12-09 16:22:36 +0900

    meshraycast

---
+++ Assets/Scripts/SceneRaycastTest.cs
@@ -0,0 +1,36 @@
+﻿using Meta.XR;
+using UnityEngine;
+
+public class SceneRaycastTest : MonoBehaviour
+{
+    [SerializeField] 
+    private EnvironmentRaycastManager _raycastManager;
+
+    [SerializeField] 
+    private Transform originTransform;
+
+    [SerializeField] private GameObject pointer;
+
+    private Ray _ray;
+    
+    private void Start()
+    {
+        _ray = new Ray(originTransform.position, originTransform.forward);
+    }
+
+    private void Update()
+    {
+        _ray.origin = originTransform.position;
+        _ray.direction = originTransform.forward;
+
+        if (_raycastManager.Raycast(_ray, out var hit))
+        {
+            if (hit.status == EnvironmentRaycastHitStatus.Hit)
+            {
+                //Debug.Log($"hit: {hit.point}, {hit.normal}");
+                pointer.transform.position = hit.point;
+                pointer.transform.rotation = Quaternion.LookRotation(hit.normal);
+            }
+        }
+    }
+}
